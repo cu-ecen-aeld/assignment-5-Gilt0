db9dd363b9d51123d11bbcb203f8538f80dfce10
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2023-07-25 05:38:24 -0700

    assignment-4-part-2-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f70876090ce0c210d34a7aab16fa8ac72d8a2d0f
+AESD_ASSIGNMENTS_VERSION = 06170d50b3251587641b9314ed421cb225169c7f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
