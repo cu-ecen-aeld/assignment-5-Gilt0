0b0ae2b66a0efa30e6d11e5d81ddfb8a6baaa2fb
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2023-07-27 02:47:12 -0700

    debug

--- full-test.sh
+++ full-test.sh
@@ -20,6 +20,7 @@ echo "Running test with user $(whoami)"
 
 set +e
 
+
 # If there's a configuration for the assignment number, use this to look for
 # additional tests
 if [ -f conf/assignment.txt ]; then
